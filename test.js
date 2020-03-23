31ffccb8d8780f563e394b0df02c6c77ecaf7c71
Peeter Piegaze <61758048+peeter-piegaze-sonarsource@users.noreply.github.com>
2020-03-23 14:28:53 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 num1 = 1.5
 num2 = 6.3
 sum = float(num1) + float(num2 # Missing parenthesis here
-printttttt('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
